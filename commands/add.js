2f29dd0aaebaf6751c8927c7da6f5dbe04d4f5b7
mtso <mtso@tuta.io>
2017-04-15 22:42:17 -0700

    Fix Day counting

--- commands/add.js
+++ commands/add.js
@@ -1,37 +1,44 @@
 const Day = require('../models/Day')
 const EphemeralError = require('../utils/ephemeral-error')
 
-const MAX_DAYS = 29;
+const MAX_DAYS = 30;
 
 function add(body, args) {
   return new Promise((resolve, reject) => {
-    var days = Day.where({user_id: body.user_id}).count()
-
     if (!args.trim()) {
       let err = new EphemeralError('A title is required to add a new day.')
       return reject(err)
     }
 
-    if (days > MAX_DAYS) {
-      let response = {
-        response_type: 'ephemeral',
-        text: 'You can only have ' + MAX_DAYS + ' days planned.',
+    Day.where({_user_id: body.user_id})
+       .count(handleCount)
+
+    function handleCount(err, count) {
+      if (err) {
+        return reject(new EphemeralError(err.toString()))
       }
-      return reject(response)
+
+      if (count > MAX_DAYS) {
+        let response = {
+          response_type: 'ephemeral',
+          text: 'You can only have ' + MAX_DAYS + ' days planned.',
+        }
+        return reject(response)
+      }
+
+      let day = new Day({
+        _user_id: body.user_id,
+        title: args,
+      })
+
+      day.save((err, data) => {
+        let response = {
+          response_type: 'ephemeral',
+          text: 'Added ' + data.title
+        }
+        resolve(response)
+      })
     }
-
-    let day = new Day({
-      _user_id: body.user_id,
-      title: args,
-    })
-
-    day.save((err, data) => {
-      let response = {
-        response_type: 'ephemeral',
-        text: 'Added ' + data.title
-      }
-      resolve(response)
-    })
   })
 }
 
