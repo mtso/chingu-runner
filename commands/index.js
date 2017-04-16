795df015bccaa5450d7cb41009ac494dd6a576ec
mtso <mtso@tuta.io>
2017-04-15 23:42:23 -0700

    Update commands hash

--- commands/index.js
+++ commands/index.js
@@ -2,15 +2,8 @@ const add = require('./add')
 const list = require('./list')
 const done = require('./done')
 
-// Slack API test commands
-const echo = require('./echo')
-const shout = require('./shout')
-
 module.exports = {
   add,
   list,
   done,
-
-  echo,
-  shout,
 }
