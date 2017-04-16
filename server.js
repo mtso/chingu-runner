a4f6273414bd085390bc369cd744a16c303e206a
mtso <mtso@tuta.io>
2017-04-15 19:29:36 -0700

    Try team post

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const indexPath = path.resolve(__dirname, 'public')
 
 const authController = require('./controllers/auth')
 
+let Team = require('./models/Team')
+
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -32,6 +34,16 @@ app.get('/', function(req, res) {
 })
 
 app.post('/command', function(req, res) {
+  Team.find({_id: req.body.team_id}, function(err, data) {
+    if (err || data.length === 0) {
+      return res.json({
+        error: 'Team not found.'
+      })
+    } else {
+      res.json(data)
+      request.post(data.webhook, {form: {text: req.body.user_name + ' says hello~'}})
+    }
+  })
   console.log(req.body)
   res.json(data)
 })
