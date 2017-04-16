53db3211a16f81a8bea797a39e652fcd2d891a9b
mtso <mtso@tuta.io>
2017-04-15 20:18:28 -0700

    Add shout command and Promisify commands

--- controllers/commands.js
+++ controllers/commands.js
@@ -1,18 +1,44 @@
+const request = require('request')
+const Team = require('../models/Team')
+
 function echo(body, args) {
-  let response = {
-    response_type: 'ephemeral',
-    text: 'Hello, you tried echo on ' + args
-  }
-  return response
-  // if (req.body.text === 'whisper') {
-  //   let d = {
-  //     response_type: 'ephemeral',
-  //     text: 'Hello, world~'
-  //   }
-  //   return res.json(d)
-  // }
+  return new Promise((resolve, reject) => {
+    let response = {
+      response_type: 'ephemeral',
+      text: 'Hello, you tried echo on ' + args
+    }
+    return resolve(response)
+  })
+}
+
+function shout(body, args) {
+  return new Promise((resolve, reject) => {
+
+    Team.findOne({_id: body.team_id}, '_id webhook', function(err, team) {
+      if (err || !team) {
+        return {
+          error: 'Team not found.'
+        }
+      } else {
+        request.post({
+            url: team.webhook,
+            body: { text: body.user_name + ' shouts HELLO~' },
+            json: true
+          }
+          // Need to change this so that if access is revoked, we remove the team webhook
+          , (err, data) => console.log(err, data.body))
+
+        let response = {
+          response_type: 'ephemeral',
+          text: 'you shouted into the channel!'
+        }
+        return resolve(response)
+      }
+    })
+  })
 }
 
 module.exports = {
-  echo
+  echo,
+  shout
 }
