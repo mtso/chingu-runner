800b5f9bd4f3cc393ee760bbb13565122a8086de
mtso <mtso@tuta.io>
2017-04-16 19:15:35 -0700

    Refactor Add command

--- commands/add.js
+++ commands/add.js
@@ -1,67 +1,64 @@
 const Day = require('../models/Day')
 const EphemeralError = require('../utils/ephemeral-error')
 const User = require('../models/User')
-
-const MAX_DAYS = 30;
-
-function checkUser(body, callback) {
-  let user = new User({
-    _id: body.user_id,
-    user_name: body.user_name,
-    _team_id: body.team_id,
-  })
-
-  user.save((err, data) => {
-    if (err && err.code !== 11000) {
-      return callback(err, data)
-    }
-    callback(null, data)
-  })
-}
+const config = require('../config.json')
 
 function add(body, args) {
-  // Add user if user doesn't exist
   return new Promise((resolve, reject) => {
-    checkUser(body, (err, data) => {
-      if (err) {
-        reject(new EphemeralError(err))
-      }
+    if (!args.trim()) {
+      let err = new EphemeralError('A title is required to add a new day.')
+      return reject(err)
+    }
 
-      if (!args.trim()) {
-        let err = new EphemeralError('A title is required to add a new day.')
-        return reject(err)
-      }
+    return checkUser(body)
 
+    // Add user if user doesn't exist
+    function checkUser(body) {
+      let user = new User({
+        _id: body.user_id,
+        user_name: body.user_name,
+        _team_id: body.team_id,
+      })
+
+      user.save(countDays)
+    }
+
+    function countDays(err, _) {
+      if (err && err.code !== 11000) {
+        return reject(new EphemeralError(err))
+      }
       Day.where({_user_id: body.user_id})
          .count(handleCount)
+    }
 
-      function handleCount(err, count) {
-        if (err) {
-          return reject(new EphemeralError(err.toString()))
-        }
-
-        if (count > MAX_DAYS) {
-          let response = {
-            response_type: 'ephemeral',
-            text: 'You can only have ' + MAX_DAYS + ' days planned.',
-          }
-          return reject(response)
-        }
-
-        let day = new Day({
-          _user_id: body.user_id,
-          title: args,
-        })
-
-        day.save((err, data) => {
-          let response = {
-            response_type: 'ephemeral',
-            text: 'Added ' + data.title
-          }
-          resolve(response)
-        })
+    function handleCount(err, count) {
+      if (err) {
+        return reject(new EphemeralError(err.toString()))
       }
-    })
+
+      if (count > config.MAX_DAYS) {
+        let response = {
+          response_type: 'ephemeral',
+          text: 'You can only have ' + config.MAX_DAYS + ' days planned.',
+        }
+        return reject(response)
+      }
+
+      let day = new Day({
+        _user_id: body.user_id,
+        title: args,
+      })
+
+      day.save(handleSave)
+    }
+
+    function handleSave(err, data) {
+      let response = {
+        response_type: 'ephemeral',
+        text: 'Added ' + data.title
+      }
+      resolve(response)
+    }
   })
 }
 
