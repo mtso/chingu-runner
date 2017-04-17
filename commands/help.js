712dc0b75de29cfad5dc8542cc6e24f737b1025c
mtso <mtso@tuta.io>
2017-04-17 10:33:28 -0700

    Add help command

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,39 @@
 const EphemeralMessage = require('../utils/ephemeral-message')
 
 function help(body, args) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, _) => {
     // Add help text (or this is just the same as inputting an unrecognized command)
-    let message = 'The available commands are: add, done, list, and help'
-    resolve(new EphemeralMessage(message))
+    // let message = 'The available commands are: add, done, list, and help'
+    // resolve(new EphemeralMessage(message))
+
+    let attachments = [
+      {
+        color: 'yellow',
+        text: 'Chingu Runner commands:',
+        fields: [
+          {
+            title: '`/run add [title]` adds an item to your speedrun list'
+          },
+          {
+            title: '`/run done` marks today\'s item as complete'
+          },
+          {
+            title: '`/run list` prints a list of all your items'
+          },
+          {
+            title: '`/run pop` removes the last item from your list'
+          },
+          {
+            title: '`/run undone` marks the latest done item as not done'
+          },
+        ]
+      }
+    ]
+    let response = {
+      response_type: 'ephemeral',
+      attachments,
+    }
+    resolve(response)
   })
 }
 
