192956472fdfd12fc617fc2b4b0ebb0e03eb9fc6
mtso <mtso@tuta.io>
2017-04-16 23:57:12 -0700

    This is a combination of 8 commits. Prepare done/undone commands to support daily-digest job
    Add test digest command
    
    Stub job file

---
+++ commands/testdigest.js
@@ -0,0 +1,50 @@
+const Team = require('../models/Team')
+const User = require('../models/User')
+const Day = require('../models/Day')
+
+const EphMessage = require('../utils/ephemeral-message')
+
+// get team webhooks
+// get users that belong to a team
+// for each user, get the last 3 tasks that they completed today
+// compile daily digest
+// for each team, make post request
+
+function testdigest() {
+  return new Promise((resolve, reject) => {
+    Team
+      .find({})
+      .exec((err, teams) => {
+        // console.log(err, teams)
+
+        teams.forEach(sendDigestForTeam)
+      })
+    resolve(new EphMessage('received command'))
+  })
+}
+
+function sendDigestForTeam(team) {
+  User.find({})
+    .exec((err, users) => {
+      console.log(users)
+    })
+  User.find({_team_id: team._id})
+    .exec((err, users) => {
+      console.log(users)
+    })
+
+}
+
+// request.post({
+//     url: team.webhook,
+//     body: {
+//       text: '@' + body.user_name + ' finished today!',
+//       link_names: true,
+//     },
+//     json: true,
+//   }
+//   // Need to change this so that if access is revoked, we remove the team webhook
+//   , (err, data) => console.log(err, data.body)
+// )
+
+module.exports = testdigest
