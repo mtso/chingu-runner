7d31e20bc9b9fe60f0766e2dd9948e30a80601be
mtso <mtso@tuta.io>
2017-04-17 10:41:32 -0700

    Fix unquoted word

--- commands/help.js
+++ commands/help.js
@@ -27,7 +27,7 @@ function help(body, args) {
             title: '`/run undone` marks the latest done item as not done'
           },
         ],
-        mrkdn_in: ['fields', text],
+        mrkdn_in: ['fields', 'text'],
         footer: '<https://chingurunner.herokuapp.com/ | Chingu Runner>',
       }
     ]
